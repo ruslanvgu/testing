c94584509cac9861c24fc67fb913aeed475fb798
ruslanvgu <ruslanvgu@mail.ru>
2024-06-13 22:57:02 +0300

    add static library

---
+++ lib/libmessage.c
@@ -0,0 +1,6 @@
+#include "libmessage.h"
+#include <stdio.h>
+
+void printMessage() {
+    printf("Hello from the static library!\n");
+}
---
+++ lib/libmessage.h
@@ -0,0 +1,10 @@
+#ifndef LIBMESSAGE_H
+#define LIBMESSAGE_H
+
+
+
+void printMessage();
+
+
+
+#endif
