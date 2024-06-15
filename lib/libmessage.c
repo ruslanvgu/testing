df94428da098eb9cdee9dcdb1cf7895d92728fb2
ruslanvgu <ruslanvgu@mail.ru>
2024-06-15 22:27:26 +0300

    delete files

--- lib/libmessage.c
+++
@@ -1,6 +0,0 @@
-#include "libmessage.h"
-#include <stdio.h>
-
-void printMessage() {
-    printf("Hello from the static library!\n");
-}
--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,27 @@
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+#include <SQLiteCpp/SQLiteCpp.h>
+#include <SQLiteCpp/Database.h>
 
-#include "../lib/libmessage.h"
+#include <iostream>
+#pragma link "sqlitecpp.lib" 
 
-#ifdef __cplusplus
-}
-#endif
+int main() {
+    try {
+        SQLite::Database db("docs.db", SQLite::OPEN_READWRITE | SQLite::OPEN_CREATE);
 
-//func1
-//func2
-//func3
+        //db.exec("CREATE TABLE IF NOT EXISTS Documents (id INTEGER PRIMARY KEY, text TEXT, content BLOB, create_data INTEGER)");
 
-int main (int argc, char** argv) {
+        SQLite::Statement query(db, "INSERT INTO Documents(text, content, create_data) VALUES (?, ?, ?)");
+        query.bind(1, "Alice");
+        query.bind(2, "");
+        query.bind(3, 12315);
+        query.exec();
 
-    printMessage (); // using function from static library libmessage
+        std::cout << "Данные успешно добавлены в таблицу." << std::endl;
+    } catch (std::exception &e) {
+        std::cerr << "Ошибка при работе с базой данных: " << e.what() << std::endl;
+        return 1;
+    }
 
     return 0;
 }
